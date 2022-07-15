d2539b7e32be9c6eeada371ddfc686f8c3d191c2
kamausuzan197 <kamaususan197@gmail.com>
2022-07-15 06:01:35 +0300

    adding

--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -1,11 +1,16 @@
 import React from "react";
+{/*a...we attach an event listerner to an element much like we add a prop.
+b....takes a callback function as a value
 
+
+
+*/}
 function Tickler() {
   function tickle() {
     console.log("Teehee!");
   }
 
-  return <button>Tickle me!</button>;
+  return <button onClick={"tickle"}>Tickle me!</button>;
 }
 
 export default Tickler;
